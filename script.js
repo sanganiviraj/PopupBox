d1337707e610cf0e182c77761b10a459b3ecdf74
Sangani Viraj <sanganiviraj263@gmail.com>
2025-02-11 15:45:47 +0530

    change in space

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
     let submitBtn = document.getElementById("submitBtn")
-    let popupOpen=document.querySelector(".popup-overlay")
-    let popupClose=document.querySelector(".popup-closeBtn")
-    let currentDate=document.querySelector(".current-date")
+    let popupOpen = document.querySelector(".popup-overlay")
+    let popupClose = document.querySelector(".popup-closeBtn")
+    let currentDate = document.querySelector(".current-date")
+
+
+
+
+
+
 
     submitBtn.addEventListener("click", function () {
         popupOpen.classList.add("fade-in");
-        popupOpen.style.display="block";
-        submitBtn.style.display = "none"; 
+        popupOpen.style.display = "block";
+        submitBtn.style.display = "none";
         // today=new Date()
         // console.log(today.toLocaleDateString());
-        
-        const today = new Date();
-        let dd=today.getDate()
-        let mm=today.getMonth()+1
-        let yyyy=today.getFullYear()
 
-        newDate=`${dd}/${mm}/${yyyy}`
-        currentDate.textContent=newDate
+        const today = new Date();
+        let dd = today.getDate()
+        let mm = today.getMonth() + 1
+        let yyyy = today.getFullYear()
+
+        newDate = `${dd}/${mm}/${yyyy}`
+        currentDate.textContent = newDate
     })
-    popupClose.addEventListener("click",function(){
-        popupOpen.style.display="none";
+    popupClose.addEventListener("click", function () {
+        popupOpen.style.display = "none";
         submitBtn.style.display = "block";
         // popupOpen.classList.remove("fade-in");  
     })
-    
+
 });
